dd41052593e17499a10fc461f907387995b0eaf5
ravindugamage <rgam0005@student.monash.edu>
2025-03-21 00:18:35 +1100

    add HashMap class with basic structure and methods

--- hashmap/main.cpp
+++ hashmap/main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
+#include <array>
 #include <list>
+#include <ostream>
+#include <utility>
 
+template <typename K, typename V> class HashMap
+{
+  private:
+	static const int buckets{ 10 };
+	std::array<std::list<std::pair<K, V>>, buckets> table;
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
-}
+  public:
+	bool isEmpty() const;
+	int hashFunction();
+	void insert(const K key, const V value);
+	void remove(const K key);
+	V& getItem(const K key);
 
+	friend std::ostream& operator<<(std::ostream& out,
+									const HashMap& map); // For printing table with primitive types
+};
+
+int main() { return 0; }
